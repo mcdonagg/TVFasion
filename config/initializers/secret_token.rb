d99315b65c7c27b979b4d4428cd5888d15f91722
mcdonagg <mcdonagg@gmail.com>
2014-01-17 21:36:31 -0500

    Created rails and added actor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TVFashion::Application.config.secret_key_base = 'da8b1cc2cd5c714de8b31bd74af51e8a051e74ad5bba9097fb0d12d3c4a140ea26219b8212581824350ec21d436f8a821e3df9f5b336d663c1cca1e6cf7086cd'
